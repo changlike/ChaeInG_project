f77e58386da8b6ef8aef8add50c0277aea5409c8
changlike <csong0715@gmail.com>
2026-07-29 15:12:29 +0900

    favorite 라우터 등록

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 # FastAPI 도구에서 서버를 만드는 도구(FastAPI 클래스)를 가져옴
 from fastapi import FastAPI
-from app.api import auth, profile, product
+from app.api import auth, profile, product, favorite
 
 # 본 프로젝트의 서버를 하나 만듦
 # 앞으로 이 app이라는 이름으로 서버에 여러 기능(API)을 하나씩 추가할 예정
@@ -9,7 +9,10 @@ app = FastAPI()
 # auth.py에 있는 API들을 "/api/auth"라는 주소 밑에 연결
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
 app.include_router(profile.router, prefix="/api/profile", tags=["profile"])
-app.include_router(product.router, prefix="/api/prodcus", tags=["product"])
+app.include_router(product.router, prefix="/api/products", tags=["product"])
+
+app.include_router(favorite.router, prefix="/api/favorites", tags=["favorite"])
+
 # @app.get("/"): "누군가 우리 서버의 기본 주소('/')로 GET 요청(조회 요청)을 보내면"의 의미
 @app.get("/")
 def read_root():
